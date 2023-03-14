28dd4bf6bfb02615ab2dc6ed513d43da40f471f0
Dominic Lizarraga <70678718+dominiclizarraga@users.noreply.github.com>
2023-03-14 16:00:27 -0700

    mocks, stubs and fake objects

---
+++ test_doubles/test/test_helper.rb
@@ -0,0 +1,5 @@
+require 'minitest/autorun'
+require "minitest/pride"
+# The Minitest runner is activated when you `require minitest/autorun`` in
+# your code. It's responsible for loading the Ruby code needed to run
+# Minitest and kicking off the test run with the Minitest.autorun method.
