a62f57470846a802fffc306708c9eeee84a04eff
Dominic Lizarraga <70678718+dominiclizarraga@users.noreply.github.com>
2023-03-14 20:46:41 -0700

    mixins testing (coupled and non-coupled)

---
+++ greetable/test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require "minitest/pride"
+require 'greetable'
+# The Minitest runner is activated when you `require minitest/autorun`` in
+# your code. It's responsible for loading the Ruby code needed to run
+# Minitest and kicking off the test run with the Minitest.autorun method.
